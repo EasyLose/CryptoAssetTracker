512ef31220c77e790a8199aec43649e9bc71bbc5
EasyLose <160155157+EasyLose@users.noreply.github.com>
2024-04-15 12:55:33 +0200

    improving variables

--- deploy_contracts.js
+++ deploy_contracts.js
@@ -1,26 +1,28 @@
 const HDWalletProvider = require('@truffle/hdwallet-provider');
 require('dotenv').config();
-const mnemonic = process.env.MNEMONIC;
-const infuraKey = process.env.INFURA_API_KEY;
+
+const walletMnemonic = process.env.MNEMONIC;
+const infuraApiKey = process.env.INFURA_API_KEY;
+
 module.exports = {
   networks: {
     development: {
-      host: "127.0.0.1",     
-      port: 7545,            
-      network_id: "*",       
+      host: "127.0.0.1",
+      port: 7545,
+      network_id: "*", 
     },
     ropsten: {
-      provider: () => new HDWalletProvider(mnemonic, `https://ropsten.infura.io/v3/${infuraKey}`),
-      network_id: 3,         
-      gas: 5500000,          
-      confirmations: 2,      
-      timeoutBlocks: 200,    
-      skipDryRun: true       
+      provider: () => new HDWalletProvider(walletMnemonic, `https://ropsten.infura.io/v3/${infuraApiKey}`),
+      network_id: 3, 
+      gasLimit: 5500000, 
+      confirmationsRequired: 2, 
+      timeoutBlocks: 200,
+      skipDryRun: true
     }
   },
   compilers: {
     solc: {
-      version: "^0.8.0",    
+      version: "^0.8.0", 
     }
   }
 };
