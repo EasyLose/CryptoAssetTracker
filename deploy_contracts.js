267dc2be1651fb1aa8a75b8884d64f97256e355b
EasyLose <160155157+EasyLose@users.noreply.github.com>
2024-04-15 13:00:00 +0200

    optimization improvement

--- deploy_contracts.js
+++ deploy_contracts.js
@@ -1,1 +1,24 @@
+let numbers = [1, 2, 3, 4, 5];
+let doubled = [];
+
+for (let i = 0; i < numbers.length; i++) {
+  doubled.push(numbers[i] * 2);
+}
 ```
+
+```javascript
+let numbers = [1, 2, 3, 4, 5];
+let doubled = numbers.map(n => n * 2);
+```
+
+```javascript
+for (let i = 0; i < myArray.length; i++) {
+  // Operation on myArray[i]
+}
+```
+
+```javascript
+const length = myArray.length;
+for (let i = 0; i < length; i++) {
+  // Operation on myArray[i]
+}
